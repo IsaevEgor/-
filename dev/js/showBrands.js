201b2f20721b3d6d2bc6c5c205d6c2f97bc60313
IsaevEgor <isaevegor.mail@gmail.com>
2023-01-10 21:03:55 +0300

    small fix

--- dev/js/showBrands.js
+++ dev/js/showBrands.js
@@ -5,11 +5,11 @@ function showBrands(data) {
 	brandBlock.innerHTML = null;
 	const brands = data.map(position => position.brand);
 	const myBrandValue = myBrandInput.value;
-	brands.forEach(brand => {
+	brands.forEach((brand, index) => {
 		if (myBrandValue === brand) {
-			brandBlock.innerHTML += `<li class="brands__item _my-brand">${brand}</li>`
+			brandBlock.innerHTML += `<li class="brands__item _my-brand">${index+1}.${brand}</li>`
 		} else {
-			brandBlock.innerHTML += `<li class="brands__item">${brand}</li>`
+			brandBlock.innerHTML += `<li class="brands__item">${index+1}.${brand}</li>`
 		}
 	});
 }
